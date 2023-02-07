15eaf3799e6b0dea3c81e9b24dec588fa35949f1
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-07 13:20:00 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SPVM::File::Copy
+
+SPVM::File::Copy is a SPVM module.
+
---
+++ lib/SPVM/File/Copy.pm
@@ -0,0 +1,45 @@
+package SPVM::File::Copy;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::File::Copy - Short Description
+
+=head1 Description
+
+C<SPVM::File::Copy> is the C<File::Copy> class in L<SPVM> language.
+
+=head1 Usage
+
+  use File::Copy;
+  
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2023-2023 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
